f7aa92a52efdd8b838d0cd5944e682f598680f12
AGBAHOLOU Romaric Armel <romaric-armel.agbaholou@etudiant.univ-rennes.fr>
2025-04-14 21:49:58 +0000

    FINALIZING USERS LIST VIEW & EVENTS LIST VIEW USING TEMPLATE AND ROUTERS

--- src/app/components/layout/main-content/main-content.component.ts
+++ src/app/components/layout/main-content/main-content.component.ts
@@ -1,54 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TableModule } from 'primeng/table';
-import { FooterComponent } from '../footer/footer.component';
-import { HttpClient } from '@angular/common/http';
-
-interface User {
-  id: number;
-  nom: string;
-  prenom: string;
-  email: string;
-  role: string;
-}
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
   imports: [
     CommonModule,
-    TableModule,
-    FooterComponent
+    RouterOutlet
   ],
-  templateUrl: './main-content.component.html',
+  template: `
+    <div class="main-content-wrapper">
+      <div class="scrollable-content">
+        <router-outlet></router-outlet>
+      </div>
+    </div>
+  `,
   styleUrl: './main-content.component.css'
 })
-export class MainContentComponent implements OnInit {
-  users: User[] = [];
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    this.loadUsers();
-  }
-
-  loadUsers() {
-    this.http.get<User[]>('/api/users').subscribe({
-      next: (data) => {
-        this.users = data;
-      },
-      error: (error) => {
-        console.error('Error fetching users:', error);
-        // Here you could add error handling, like showing a message to the user
-      }
-    });
-  }
-
-  onEdit(user: User) {
-    console.log('Edit user:', user);
-  }
-
-  onDelete(user: User) {
-    console.log('Delete user:', user);
-  }
-}
+export class MainContentComponent {}
