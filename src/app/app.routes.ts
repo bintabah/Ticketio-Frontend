5ea98fd27b83e101a54be566e7b63ac10f188519
AGBAHOLOU Romaric Armel <romaric-armel.agbaholou@etudiant.univ-rennes.fr>
2025-04-15 09:40:04 +0000

    MERGE ARTISTS, USERS AND EVENTS | CREATE, READALL, UPDATE ONE

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { UsersComponent } from './components/views/users/users.component';
 import { EventsComponent } from './components/views/events/events.component';
-import { ArtistsComponent } from './components/entities/artists/artists.component';
+import { ArtistsComponent } from './components/views/artists/artists.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'users', pathMatch: 'full' },
