01ee519e3ddb07a2a32338ffa0dc0307ac3a058b
Bah Hadja Sory Binta <hadja-sory-binta.bah@etudiant.univ-rennes.fr>
2026-05-29 14:35:44 +0200

    fix: correct Render backend URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ticketio-backend.onrender.com',
+  apiUrl: 'https://ticketio-backend-6mvm.onrender.com',
   stripePublicKey: 'pk_test_51KXX0QGOeuivamSHx5bwQigBxklvV4d84wjTRdUiFg8mBzimtf3oiFOgbzU2dFrvgbZkRM8epO8S3K9u3wK9k7cO00tjwOUAVW'
 };
